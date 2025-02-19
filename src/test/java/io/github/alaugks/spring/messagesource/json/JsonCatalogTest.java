b79aae864bef528a1b1342f34355e44471e4f5fc
André Laugks <al@pb82.de>
2025-02-19 21:41:29 +0100

    Test IOException

--- src/test/java/io/github/alaugks/spring/messagesource/json/JsonCatalogTest.java
+++ src/test/java/io/github/alaugks/spring/messagesource/json/JsonCatalogTest.java
@@ -1,10 +1,19 @@
 package io.github.alaugks.spring.messagesource.json;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import io.github.alaugks.spring.messagesource.catalog.records.TransUnitInterface;
+import io.github.alaugks.spring.messagesource.catalog.records.TranslationFile;
 import io.github.alaugks.spring.messagesource.catalog.resources.LocationPattern;
 import io.github.alaugks.spring.messagesource.catalog.resources.ResourcesLoader;
+import io.github.alaugks.spring.messagesource.json.exception.JsonResourceMessageSourceIOException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import org.junit.jupiter.api.Test;
@@ -26,6 +35,28 @@ class JsonCatalogTest {
 
 		assertEquals("Postcode", this.findInTransUnits(transUnits, "en", "postcode"));
 		assertEquals("Postleitzahl", this.findInTransUnits(transUnits, "de", "postcode"));
+	}
+
+	@Test
+	void test_IOException() {
+
+		try {
+			InputStream inputStreamMock = mock(InputStream.class);
+			when(inputStreamMock.readAllBytes()).thenThrow(IOException.class);
+
+			List<TranslationFile> list = new ArrayList<>();
+			list.add(new TranslationFile(
+				"domain",
+				Locale.forLanguageTag("en"),
+				inputStreamMock
+			));
+
+			var catalog = new JsonCatalog(list);
+
+			assertThrows(JsonResourceMessageSourceIOException.class, catalog::build);
+		} catch (IOException e) {
+			fail(e.getMessage());
+		}
 	}
 
 	private String findInTransUnits(List<TransUnitInterface> transUnits, String locale, String code) {
